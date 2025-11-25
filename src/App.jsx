eae80a3606864ab8e16a7f1cef042817b8c10c64
bebaz <limaafonso57@gmail.com>
2025-11-25 00:06:01 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className={'heroWrapper'}>
+        <div className={'profile'}>
+            <img/>
+            <h1>Afonso Lima</h1>
+            <p>BiriBiBabidiBoo</p>
+            <div className={'techWrapper'}>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default App
