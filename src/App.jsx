d3edb7fa2eff451900c2455ab741d15c4aaa1e64
bebaz <limaafonso57@gmail.com>
2025-11-27 22:55:56 +0000

    Minor changes specially on color and font

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className={'heroWrapper'}>
         <div className={'profile'}>
-            <img/>
             <h1>Afonso Lima</h1>
+            <div className={'socials'}> </div>
             <p>Full-Stack Developer specialized in modern frontend and user-centric UI. I build things with a clear
                 purpose, no gimmicks. Straightforward, original. Driven by creator mentality and good ideas.</p>
             <TechStack/>
