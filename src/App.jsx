01f774794a95865b79beb5fa42bd74e5c2b9f5e9
bebaz <limaafonso57@gmail.com>
2025-11-27 23:01:10 +0000

    Minor changes specially on color and font

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
             <h1>Afonso Lima</h1>
             <div className={'socials'}> </div>
             <p>Full-Stack Developer specialized in modern frontend and user-centric UI. I build things with a clear
-                purpose, no gimmicks. Straightforward, original. Driven by creator mentality and good ideas.</p>
+                purpose, no gimmicks, straightforward, original. Driven by a creator mentality and good ideas.</p>
             <TechStack/>
         </div>
         <div className={'todo'}>
